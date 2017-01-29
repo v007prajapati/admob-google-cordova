3985e6f619cbff28e4bf0d379cbbcc043439cb11
Vishal Prajapati <vishal.p@techmanha.com>
2017-01-29 10:01:50 +0530

    updated ad unit id

--- src/android/AdMobAds.java
+++ src/android/AdMobAds.java
@@ -69,8 +69,8 @@ public class AdMobAds extends CordovaPlugin implements IConnectivityChange {
     public static final String BANNER = "banner";
 
     private static final boolean CORDOVA_4 = Integer.valueOf(CordovaWebView.CORDOVA_VERSION.split("\\.")[0]) >= 4;
-    private static final String DEFAULT_AD_PUBLISHER_ID = "ca-app-pub-8440343014846849/3119840614";
-    private static final String DEFAULT_INTERSTITIAL_PUBLISHER_ID = "ca-app-pub-8440343014846849/4596573817";
+    private static final String DEFAULT_AD_PUBLISHER_ID = "ca-app-pub-4208163364905319/7243085585";
+    private static final String DEFAULT_INTERSTITIAL_PUBLISHER_ID = "ca-app-pub-4208163364905319/5389419181";
     private static final String DEFAULT_TAPPX_ID = "/120940746/Pub-2700-Android-8171";
 
     /* Cordova Actions. */
@@ -368,13 +368,13 @@ public class AdMobAds extends CordovaPlugin implements IConnectivityChange {
                     _publisherId = DEFAULT_TAPPX_ID;
                 }
             } else if (!isGo2TappxInBannerBackfill) {
-                _publisherId = "ca-app-pub-8440343014846849/3119840614";
+                _publisherId = "ca-app-pub-4208163364905319/7243085585";
                 isGo2TappxInBannerBackfill = true;
             } else {
                 _publisherId = DEFAULT_TAPPX_ID;
             }
         } else if (isBackFill && !isGo2TappxInBannerBackfill) {
-            _publisherId = "ca-app-pub-8440343014846849/3119840614";
+            _publisherId = "ca-app-pub-4208163364905319/7243085585";
             isGo2TappxInBannerBackfill = true;
         } else if (isBackFill) {
             _publisherId = DEFAULT_TAPPX_ID;
@@ -605,13 +605,13 @@ public class AdMobAds extends CordovaPlugin implements IConnectivityChange {
                     _interstitialAdId = DEFAULT_TAPPX_ID;
                 }
             } else if (!isGo2TappxInIntesrtitialBackfill) {
-                _interstitialAdId = "ca-app-pub-8440343014846849/4596573817";
+                _interstitialAdId = "ca-app-pub-4208163364905319/5389419181";
                 isGo2TappxInIntesrtitialBackfill = true;
             } else {
                 _interstitialAdId = DEFAULT_TAPPX_ID;
             }
         } else if (isBackFill && !isGo2TappxInIntesrtitialBackfill) {
-            _interstitialAdId = "ca-app-pub-8440343014846849/4596573817";
+            _interstitialAdId = "ca-app-pub-4208163364905319/5389419181";
             isGo2TappxInIntesrtitialBackfill = true;
         } else if (isBackFill) {
             _interstitialAdId = DEFAULT_TAPPX_ID;
